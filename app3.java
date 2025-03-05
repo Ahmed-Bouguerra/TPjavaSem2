ca787e522abb0e3cfdcbd6a779d277c19865c46e
Ahmed-Bouguerra <ahmedbouguerraahmed@gmail.com>
2025-03-05 21:47:24 +0100

    Add files via upload

---
+++ app3.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+public class app3 {
+public static void main(String[] args) {
+    Scanner sr=new Scanner (System.in);
+    int nl , nc;
+do { 
+    System.out.print("entrez le nombre de lignes (1<=nl<=20):");
+    nl=sr.nextInt();
+
+} while (nl<1 || nl>20);
+do { 
+    System.out.println("entrez le nombre de colonnes (1<=nl<=30):");
+    nc=sr.nextInt();
+
+} while (nc<1 || nc>30);
+
+int[][] M= new int[nl][nc];
+int[] T= new int [nl];
+
+System.out.println("entrez les elements de la matrice");
+for (int i=0; i<nl; i++){
+    for (int j=0; j<nc; j++){
+        System.out.print("M["+i+"]["+j+"]=");
+        M[i][j]= sr.nextInt();
+
+    }
+}
+for (int i=0;i<nl; i++){
+    int somme=0;
+    int j;
+    for (j =0; j<nc;j++){
+        somme +=M[i][j];
+    }
+    T[i] = somme;
+}
+int min = T[0], max = T[0];
+for (int i= 1; i<nl;i++){
+    if (T[i]<min) {min=T[i];}
+    if (T[i]>max) {max= T[i];}
+}
+
+System.out.println("\nSommes des lignes :");
+for (int i= 0; i <nl; i++){
+    System.out.println("ligne"+(i+1)+":"+T[i]);
+
+}
+System.out.println("somme minimale :"+min);
+System.out.println("somme maximale :"+max);
+sr.close();
+
+}
+}
